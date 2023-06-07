70d08aed9e22fd19a8f10829e153f1897f8682cf
Martin Penckert <martin.penckert@gmail.com>
2023-06-07 10:46:35 +0200

    Passes 01-parse test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,8 @@
 use proc_macro::TokenStream;
+use syn::{self, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let _ = syn::parse_macro_input!(input as DeriveInput);
+    TokenStream::new()
 }
