5581f7cbd56c3bd9697a19489f18883fdbbe286e
Martin Penckert <martin.penckert@gmail.com>
2023-06-07 14:39:45 +0200

    passes 02-create-builder test

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,34 @@
 use proc_macro::TokenStream;
-use syn::{self, DeriveInput};
+use proc_macro2::Span;
+use quote::quote;
+use syn::{self, DeriveInput, Ident};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = syn::parse_macro_input!(input as DeriveInput);
-    TokenStream::new()
+    let ast = syn::parse_macro_input!(input as DeriveInput);
+
+    let struct_ident = &ast.ident;
+    let builder_struct_ident = Ident::new(&format!("{}Builder", &struct_ident), Span::call_site());
+
+    let gen = quote! {
+        pub struct #builder_struct_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #struct_ident {
+            pub fn builder() -> #builder_struct_ident {
+                #builder_struct_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    gen.into()
 }
