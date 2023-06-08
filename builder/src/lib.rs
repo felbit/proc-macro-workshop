164653b2635cbebeabe42898e00b5e920e0ef0ab
Martin Penckert <martin.penckert@gmail.com>
2023-06-08 07:25:44 +0200

    passes 03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,23 +1,17 @@
 use proc_macro::TokenStream;
-use proc_macro2::Span;
+use proc_macro2::Ident;
 use quote::quote;
-use syn::{self, DeriveInput, Ident};
+use syn::{self, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let ast = syn::parse_macro_input!(input as DeriveInput);
 
     let struct_ident = &ast.ident;
-    let builder_struct_ident = Ident::new(&format!("{}Builder", &struct_ident), Span::call_site());
+    let builder_struct_ident =
+        Ident::new(&format!("{}Builder", &struct_ident), struct_ident.span());
 
     let gen = quote! {
-        pub struct #builder_struct_ident {
-            executable: Option<String>,
-            args: Option<Vec<String>>,
-            env: Option<Vec<String>>,
-            current_dir: Option<String>,
-        }
-
         impl #struct_ident {
             pub fn builder() -> #builder_struct_ident {
                 #builder_struct_ident {
@@ -26,6 +20,35 @@ pub fn derive(input: TokenStream) -> TokenStream {
                     env: None,
                     current_dir: None,
                 }
+            }
+        }
+
+        pub struct #builder_struct_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #builder_struct_ident {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
             }
         }
     };
